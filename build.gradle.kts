de39b642bd6683d670cacf7d0afc4d852b7ed0b5
Hakim <abdelhakim.nahili@etu.univ-amu.fr>
2025-09-26 16:49:47 +0200

    fixed problem of files

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.20.0")
 }
 
 tasks.test {
