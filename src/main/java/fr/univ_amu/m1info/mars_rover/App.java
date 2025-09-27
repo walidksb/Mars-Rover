459353bf3b2eb443f6b71d85d2a7ef91e6b80995
Hakim <abdelhakim.nahili@etu.univ-amu.fr>
2025-09-27 18:16:04 +0200

    output.yml file creation

--- src/main/java/fr/univ_amu/m1info/mars_rover/App.java
+++ src/main/java/fr/univ_amu/m1info/mars_rover/App.java
@@ -1,8 +1,13 @@
 package fr.univ_amu.m1info.mars_rover;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SequenceWriter;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 
@@ -18,6 +23,28 @@ public class App {
                         = App.class.getResourceAsStream("/config.yml");
                 final MarsRoverInput marsRoverMarsRoverInput =
                         objectMapper.readValue(inputStream, MarsRoverInput.class);
+                System.out.println("Configuration chargée !");
+                System.out.println(marsRoverMarsRoverInput);
+
+                //Simulation
+                Coordinates c1 = new Coordinates(1, 3);
+                Position p1 = new Position(c1, Direction.NORTH);
+                MarsRoverState state1 = new MarsRoverState(false, p1);
+
+                Coordinates c2 = new Coordinates(4, 3);
+                Position p2 = new Position(c2, Direction.EAST);
+                MarsRoverState state2 = new MarsRoverState(false, p2);
+
+                final MarsRoverOutput marsRoverOutput = new MarsRoverOutput(28.0, List.of(state1, state2));
+
+                // Écriture du fichier output.yml
+                ObjectWriter writer = objectMapper.writer();
+                try (FileOutputStream fos = new FileOutputStream("output.yml");
+                     SequenceWriter sw = writer.writeValues(fos)) {
+                     sw.write(marsRoverOutput);
+                }
+
+                System.out.println("Fichier 'output.yml' genere avec succes !");
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- src/main/java/fr/univ_amu/m1info/mars_rover/MarsRoverOutput.java
+++ src/main/java/fr/univ_amu/m1info/mars_rover/MarsRoverOutput.java
@@ -2,37 +2,4 @@ package fr.univ_amu.m1info.mars_rover;
 
 import java.util.List;
 
-public class MarsRoverOutput {
-    double percentageExplored;
-    List<MarsRoverState> finalRoverStates;
-    // Constructor
-    public MarsRoverOutput(double percentageExplored, List<MarsRoverState> finalRoverStates) {
-        this.percentageExplored = percentageExplored;
-        this.finalRoverStates = finalRoverStates;
-    }
-
-    // Getters and setters
-    public double getPercentageExplored() {
-        return percentageExplored;
-    }
-
-    public void setPercentageExplored(double percentageExplored) {
-        this.percentageExplored = percentageExplored;
-    }
-
-    public List<MarsRoverState> getFinalRoverStates() {
-        return finalRoverStates;
-    }
-
-    public void setFinalRoverStates(List<MarsRoverState> finalRoverStates) {
-        this.finalRoverStates = finalRoverStates;
-    }
-
-    @Override
-    public String toString() {
-        return "MarsRoverOutput{" +
-                "percentageExplored=" + percentageExplored +
-                ", finalRoverStates=" + finalRoverStates +
-                '}';
-    }
-}
+public record MarsRoverOutput(double percentageExplored, List<MarsRoverState> finalRoverStates) {}
