9dcaf52d987b055e9c6a9958f0b8cc9aa8daeb0e
Hakim <abdelhakim.nahili@etu.univ-amu.fr>
2025-09-27 21:48:03 +0200

    add Run task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("java")
+    id("application")
+
+
+
 }
 
 group = "fr.univ_amu.m1info.mars_rover"
@@ -10,12 +14,28 @@ repositories {
 }
 
 dependencies {
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.20.0")
+
+
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+application {
+    mainClass.set("fr.univ_amu.m1info.mars_rover.simulator.App")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "fr.univ_amu.m1info.mars_rover.simulator.App"
+    }
+}
+tasks.jar {
+    archiveBaseName.set("mars-rover")
+    archiveVersion.set("")
 }
