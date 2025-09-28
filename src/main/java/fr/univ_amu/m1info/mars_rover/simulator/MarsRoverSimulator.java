66d301545eba94d00689bc7d95fe8639f0885373
Walid <walid.kesbi@gmail.com>
2025-09-28 12:04:58 +0200

    added the destroyed feature for the rectangular gridkind

--- src/main/java/fr/univ_amu/m1info/mars_rover/simulator/MarsRoverSimulator.java
+++ src/main/java/fr/univ_amu/m1info/mars_rover/simulator/MarsRoverSimulator.java
@@ -17,42 +17,55 @@ public class MarsRoverSimulator {
             boolean destroyed = false;
 
             // Exécution fictive des commandes (à améliorer plus tard)
-            for (Command cmd : rover.commands()) {
-                switch (cmd) {
-                    case MOVE -> {
-                        // exemple : avancer de 1 vers la direction actuelle
-                        Coordinates c = current.coordinates();
-                        int x = c.x();
-                        int y = c.y();
-                        switch (current.orientation()) {
-                            case NORTH -> y++;
-                            case SOUTH -> y--;
-                            case EAST -> x++;
-                            case WEST -> x--;
+            // we check if the grid kind is rectangular or tornoidal
+            GridConfiguration gridConf = input.grid();
+            if (gridConf.kind() == GridKind.RECTANGULAR) {
+                int i =0;
+                while (i < rover.commands().size() && !destroyed) {
+                    Command cmd = rover.commands().get(i);
+                    switch (cmd) {
+                        case MOVE -> {
+                            // exemple : avancer de 1 vers la direction actuelle
+                            Coordinates c = current.coordinates();
+                            int x = c.x();
+                            int y = c.y();
+                            switch (current.orientation()) {
+                                case NORTH -> y++;
+                                case SOUTH -> y--;
+                                case EAST -> x++;
+                                case WEST -> x--;
+                            }
+                            current = new Position(new Coordinates(x, y), current.orientation());
+                            // we check if the rover gets out of the grid so it will be marked as destroyed
+                            // and we stop the command
+                            if(x > 4 || y > 4){
+                                destroyed = true;
+                            }
                         }
-                        current = new Position(new Coordinates(x, y), current.orientation());
+                        case LEFT -> {
+                            // tournée à gauche
+                            Direction newOrientation = switch (current.orientation()) {
+                                case NORTH -> Direction.WEST;
+                                case SOUTH -> Direction.EAST;
+                                case EAST  -> Direction.NORTH;
+                                case WEST  -> Direction.SOUTH;
+                            };
+                            current = new Position(current.coordinates(), newOrientation);
+                        }
+                        case RIGHT -> {
+                            // tournée à droite
+                            Direction newOrientation = switch (current.orientation()) {
+                                case NORTH -> Direction.EAST;
+                                case SOUTH -> Direction.WEST;
+                                case EAST  -> Direction.SOUTH;
+                                case WEST  -> Direction.NORTH;
+                            };
+                            current = new Position(current.coordinates(), newOrientation);
+                        }
                     }
-                    case LEFT -> {
-                        // tournée à gauche
-                        Direction newOrientation = switch (current.orientation()) {
-                            case NORTH -> Direction.WEST;
-                            case SOUTH -> Direction.EAST;
-                            case EAST  -> Direction.NORTH;
-                            case WEST  -> Direction.SOUTH;
-                        };
-                        current = new Position(current.coordinates(), newOrientation);
-                    }
-                    case RIGHT -> {
-                        // tournée à droite
-                        Direction newOrientation = switch (current.orientation()) {
-                            case NORTH -> Direction.EAST;
-                            case SOUTH -> Direction.WEST;
-                            case EAST  -> Direction.SOUTH;
-                            case WEST  -> Direction.NORTH;
-                        };
-                        current = new Position(current.coordinates(), newOrientation);
-                    }
-                }
+                    i++;
+            }
+
             }
             finalStates.add(new MarsRoverState(destroyed, current));
         }
