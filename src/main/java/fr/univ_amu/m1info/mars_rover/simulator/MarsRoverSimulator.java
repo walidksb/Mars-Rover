1927279579b79386345d29b91b09e3e3a400db71
Walid <walid.kesbi@gmail.com>
2025-09-28 12:24:30 +0200

    added the calculation of the percentageExplored and fixed the destroyed feature

--- src/main/java/fr/univ_amu/m1info/mars_rover/simulator/MarsRoverSimulator.java
+++ src/main/java/fr/univ_amu/m1info/mars_rover/simulator/MarsRoverSimulator.java
@@ -11,16 +11,24 @@ public class MarsRoverSimulator {
     public MarsRoverOutput simulate(MarsRoverInput input) {
         // Exemple simulation simplifiée
         List<MarsRoverState> finalStates = new ArrayList<>();
+        // Set to keep all explored positions without repeating because its a set
+        Set<Coordinates> explored = new HashSet<>();
 
         for (RoverConfiguration rover : input.rovers()) {
             Position current = rover.position();
             boolean destroyed = false;
 
-            // Exécution fictive des commandes (à améliorer plus tard)
+            explored.add(current.coordinates());
+
+            // Exécution fictive des commandes
             // we check if the grid kind is rectangular or tornoidal
             GridConfiguration gridConf = input.grid();
+
+            // ////////////////////// RECTANGULAR /////////////////////////
             if (gridConf.kind() == GridKind.RECTANGULAR) {
                 int i =0;
+                int maxX = gridConf.width() - 1;
+                int maxY = gridConf.height() - 1;
                 while (i < rover.commands().size() && !destroyed) {
                     Command cmd = rover.commands().get(i);
                     switch (cmd) {
@@ -38,8 +46,11 @@ public class MarsRoverSimulator {
                             current = new Position(new Coordinates(x, y), current.orientation());
                             // we check if the rover gets out of the grid so it will be marked as destroyed
                             // and we stop the command
-                            if(x > 4 || y > 4){
+                            if (x < 0 || x > maxX || y < 0 || y > maxY) {
                                 destroyed = true;
+                            } else {
+                                current = new Position(new Coordinates(x, y), current.orientation());
+                                explored.add(current.coordinates());
                             }
                         }
                         case LEFT -> {
@@ -69,9 +80,9 @@ public class MarsRoverSimulator {
             }
             finalStates.add(new MarsRoverState(destroyed, current));
         }
-
-        // Pour l’instant, un pourcentage fictif
-        double percentageExplored = 28.0;
+            // Compute percentage explored based on the grid size
+            int totalCells = input.grid().width() * input.grid().height();
+            double percentageExplored = (explored.size() * 100.0) / totalCells;
 
         return new MarsRoverOutput(percentageExplored, finalStates);
     }
