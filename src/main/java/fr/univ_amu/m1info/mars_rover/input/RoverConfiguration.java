509f480fdc388610d0a8d61c061fa29a28ca59be
Walid <walid.kesbi@gmail.com>
2025-10-02 22:35:32 +0200

    added the augmented exploration feature

--- src/main/java/fr/univ_amu/m1info/mars_rover/input/RoverConfiguration.java
+++ src/main/java/fr/univ_amu/m1info/mars_rover/input/RoverConfiguration.java
@@ -3,7 +3,7 @@ package fr.univ_amu.m1info.mars_rover.input;
 import java.util.List;
 import java.util.Objects;
 
-public record RoverConfiguration(Position position, List<Command> commands) {
+public record RoverConfiguration(Position position, List<Command> commands, int explorationRadius) {
     public RoverConfiguration {
         // Vérifier que la position n'est pas nulle
         Objects.requireNonNull(position, "La position ne doit pas être null");
